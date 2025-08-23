1a8c4f6e0d9dfa56ffa4d016d6127c90e0b4e1fa
official-Cromatin <lars-winzer@web.de>
2025-08-23 18:54:28 +0200

    Improve RegEx to match missing value

--- custom_components/wilo/parsers/status.py
+++ custom_components/wilo/parsers/status.py
@@ -8,7 +8,7 @@ class StatusParser(BaseParser):
     def parse(html:str) -> dict:
         data = {}
 
-        pattern = regex.compile(r"<span[^>]*>(.*?)</span>\s*<b>(.*?)</b>", regex.DOTALL)
+        pattern = regex.compile(r"<span[^>]*>(.*?)</span>\s*<b>(.*?)(?:<br>|</b>)", regex.DOTALL)
         for match in pattern.finditer(html):
             key = match.group(1).strip().rstrip(":")
             value = match.group(2).strip()
