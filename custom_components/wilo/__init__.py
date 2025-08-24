2847adbfe0e03ae68ccf70213116ea5b2950704f
official-Cromatin <lars-winzer@web.de>
2025-08-24 16:53:21 +0200

    Add async setup entry for integration

--- custom_components/wilo/__init__.py
+++ custom_components/wilo/__init__.py
@@ -1,1 +1,45 @@
-"""Tests for the Wilo integration."""
+"""Add Wilo integration."""
+import logging
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import timedelta
+
+from .const import DOMAIN
+from .coordinator import WiloCoordinator
+from .pumps import Rain3Pump
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Rain3 Wilo from a config entry."""
+
+    logger = logging.getLogger(DOMAIN)
+
+    ip:str = entry.data["ip"]
+    model:str = entry.data["model"]
+    interval:int = entry.data["interval"]
+
+    match model:
+        case "rain3":
+            pump = Rain3Pump(ip)
+    await pump.create_device_info(hass)
+
+    coordinator = WiloCoordinator(
+        hass,
+        logger,
+        timedelta(seconds=interval),
+        f"Wilo {model} ({entry.entry_id})",
+        pump
+        )
+
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = {
+        "coordinator": coordinator,
+        "pump": pump
+    }
+
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+
+    return True
