dd1f0cf90d76dcf5d4d9eab79f92135b2dd10bb8
official-Cromatin <lars-winzer@web.de>
2025-08-24 11:55:18 +0200

    Add type hinting for Union in parsers and pumps modules

--- custom_components/wilo/parsers/__init__.py
+++ custom_components/wilo/parsers/__init__.py
@@ -1,8 +1,12 @@
 """Parsers are used to parse html documents into dictionaries."""
 
+from typing import Union
+
 from .alarm import AlarmParser
 from .base import BaseParser
 from .settings import SettingsParser
 from .status import StatusParser
+
+WiloParser = Union[AlarmParser, SettingsParser, StatusParser]
 
 __all__ = ["AlarmParser", "BaseParser", "SettingsParser", "StatusParser"]
--- custom_components/wilo/pumps/__init__.py
+++ custom_components/wilo/pumps/__init__.py
@@ -1,6 +1,10 @@
 """Pump classes group assigned sensors and define endpoint specifications."""
 
+from typing import Union
+
 from .base import BasePump
 from .rain3 import Rain3Pump
+
+PumpHandlerClass = Union[Rain3Pump]
 
 __all__ = ["BasePump", "Rain3Pump"]
