812876c3646f7fc47d4c917cdeec208aecc09de7
official-Cromatin <lars-winzer@web.de>
2025-08-24 16:50:01 +0200

    Add WiloCoordinator class to handle data updates

---
+++ custom_components/wilo/coordinator.py
@@ -0,0 +1,16 @@
+"""Coordinator to handle updates."""
+
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+
+from .pumps import PumpHandlerClass
+
+
+class WiloCoordinator(DataUpdateCoordinator):
+    """Class to regularly fetch new data."""
+    def __init__(self, hass, logger, update_interval, name, pump:PumpHandlerClass):
+        """Initialize Wilo Coordinator."""
+        super().__init__(hass, logger, update_interval=update_interval, name=name)
+        self.__pump = pump
+
+    async def _async_update_data(self):
+        return await self.__pump.update(self.hass)
