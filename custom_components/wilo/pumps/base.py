424e94eecf3e8fdfd1b8b0608e142c0eb2cd001b
official-Cromatin <lars-winzer@web.de>
2025-08-24 16:49:04 +0200

    Refactor BasePump class to improve type hinting and structure for Wilo pump integration

--- custom_components/wilo/pumps/base.py
+++ custom_components/wilo/pumps/base.py
@@ -1,18 +1,33 @@
 """Class all pumps inherit from. Implements common methods."""
-from parsers import AlarmParser, SettingsParser, StatusParser
+from abc import abstractmethod
+from typing import Any
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.device_registry import DeviceInfo
+
+from ..entities import WiloEntities
+from ..parsers import WiloParser
 
 
 class BasePump:
     """Base pump class for Wilo pump integration."""
 
-    def __init__(self, ip:str, urls:dict[str, AlarmParser | SettingsParser | StatusParser]):
-        self._ip = ip
-        self._urls = urls
+    ENTITY_MAP: dict[tuple[str, str], WiloEntities]
 
-    async def update(self, hass:HomeAssistant) -> dict:
+    def __init__(self, ip:str, model:str, urls:dict[str, WiloParser]):
+        self._ip = ip
+        self._model = model
+        self._urls = urls
+        self._unique_id:str
+        self._device_info: DeviceInfo
+
+    @abstractmethod
+    async def create_device_info(self):
+        """Creates the device info of the pump."""
+        ...
+
+    async def update(self, hass:HomeAssistant) -> dict[str, dict[str, Any]]:
         """Updates the coordinator data by fetching all urls and combining the data to one singular dictionary."""
         session = async_get_clientsession(hass)
         combined_data = {}
@@ -23,3 +38,13 @@ class BasePump:
             data = parser.parse(html)
             combined_data[path] = data
         return combined_data
+
+    @property
+    def device_info(self):
+        """Device info used by all associated entities."""
+        return self._device_info
+
+    @property
+    def unique_id(self):
+        """Unique device id."""
+        return self._unique_id
