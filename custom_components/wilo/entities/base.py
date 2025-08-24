2a7ea11194a16e96909d329462ec5558bfcec774
official-Cromatin <lars-winzer@web.de>
2025-08-24 16:52:59 +0200

    Add WiloBaseEntity class as a base entity for Wilo devices

---
+++ custom_components/wilo/entities/base.py
@@ -0,0 +1,26 @@
+"""Base entity implementation all custom entites inherit from."""
+from typing import Any
+
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+
+class WiloBaseEntity(CoordinatorEntity):
+    """Base entity for Wilo devices."""
+
+    def __init__(self, coordinator, pump, category_key:str, value_key:str):
+        super().__init__(coordinator)
+        self._pump = pump
+        self._category_key = category_key
+        self._value_key = value_key
+        self._attr_unique_id = f"{pump.unique_id}_{category_key}_{value_key}"
+
+    def get_coordinator_value(self) -> Any:
+        """Returns the coordinator value assigned to this entity."""
+        category = self.coordinator.data.get(self._category_key)
+        return category.get(self._value_key)
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        print("DEVICE", self._pump.device_info)
+        return self._pump.device_info
